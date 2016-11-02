13db9e824e6e76152b912bf042068d0fa23ce653
NoizBuster <NoizBuster@NoizBuster.com>
2016-11-02 14:43:38 +0900

    make easy to user create script, i don't need to specify category every time

--- create.sh
+++ create.sh
@@ -1,15 +1,10 @@
 #! /bin/bash
 
-cmd="python3 ./stemcell/postgen/articlegen.py -d ./raw_source/_posts/ -a noizbuster"
+cmd="python3 ./stemcell/postgen/articlegen.py -d ./raw_source/_posts/ -a noizbuster -c development"
 
 if [ -n $1 ]
 then
-    cmd="$cmd -c $1"
-fi
-
-if [ -n $2 ]
-then
-    cmd="$cmd -t $2"
+    cmd="$cmd -t $1"
 fi
 
 echo $cmd
